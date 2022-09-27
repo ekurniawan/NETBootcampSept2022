1d235d6cfae2bee8816aeea68e357c8c5ed869ab
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-09-27 10:54:17 +0700

    menambahkan get samurai

---
+++ MyBackendApp/Controllers/SamuraisController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyBackendApp.Models;
+
+namespace MyBackendApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SamuraisController : ControllerBase
+    {
+        [HttpGet]
+        public Samurai Get()
+        {
+            Samurai samurai1 = new Samurai()
+            {
+                Id = 1,Name="Tanjiro Kamado"
+            };
+            return samurai1;
+        }
+    }
+}
---
+++ MyBackendApp/Models/Samurai.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBackendApp.Models
+{
+    public class Samurai
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
