dbb3fcbf53bbabc90ee8824659bc12e5382d16bc
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-09-27 11:35:10 +0700

    menambahkan SamuraiDAL

---
+++ MyBackendApp/DAL/ISamurai.cs
@@ -0,0 +1,10 @@
+﻿using MyBackendApp.Models;
+
+namespace MyBackendApp.DAL
+{
+    public interface ISamurai
+    {
+        public IEnumerable<Samurai> GetAll();
+        public Samurai GetById(int id);
+    }
+}
---
+++ MyBackendApp/DAL/SamuraiDAL.cs
@@ -0,0 +1,27 @@
+﻿using MyBackendApp.Models;
+
+namespace MyBackendApp.DAL
+{
+    public class SamuraiDAL : ISamurai
+    {
+        private readonly IConfiguration _config;
+        public SamuraiDAL(IConfiguration config)
+        {
+            _config = config;
+        }
+        private string GetConn()
+        {
+            return _config.GetConnectionString("SamuraiConnection");
+        }
+
+        public IEnumerable<Samurai> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Samurai GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
