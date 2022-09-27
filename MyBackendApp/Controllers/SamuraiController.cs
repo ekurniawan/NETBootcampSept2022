75a6b50bbe5e4700f760f66e9a2b96670013cf9b
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-09-27 10:36:25 +0700

    Add project files.

---
+++ MyBackendApp/Controllers/SamuraiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBackendApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SamuraiController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello Samurai !";
+        }
+    }
+}
