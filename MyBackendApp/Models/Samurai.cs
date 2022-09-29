4ec5272ce08046473748adb47de3c784c574aa9b
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-09-29 11:12:52 +0700

    menambahkan proses migrasi many to many

--- MyBackendApp/Models/Samurai.cs
+++ MyBackendApp/Models/Samurai.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Quote>  Quotes { get; set; }
+        public List<Battle> Battles { get; set; }
     }
 }
